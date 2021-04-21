7603216a7bae78d3f37eada2edd0e5cf10544ab1
Josemariabecerramarin <jbecerra@elpuig.xeill.net>
2021-04-21 18:32:47 +0200

    nave

---
+++ core/src/com/jbecerra/game/Fondo.java
@@ -0,0 +1,15 @@
+package com.jbecerra.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Fondo {
+    Texture texture;
+    Fondo(){
+        texture = new Texture("fondo.jpg");
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, 0,0);
+    }
+}
